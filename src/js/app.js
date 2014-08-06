de442aaa75555d7a31c223b2b0b626391b43b2e1
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-08-06 16:06:38 -0300

    Agregando angularjs.

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,46 @@
-var myUrl = 'tiles2/{z}/{x}/{y}.png';
-var my = new L.TileLayer(myUrl, {maxZoom: 4});
+var app = angular.module('app', ["leaflet-directive"]);
 
-//var center ,12
 
-var map = new L.Map('map', {
-  zoom: 1,
-  layers: [my],
-  center: [-20.036, 38.9013],
+app.controller("MainCtrl", function MainCtrl($scope) {
+  $scope.data = {};
+  $scope.data.markers = {
+            osloMarker: {
+                lat: 59.91,
+                lng: 10.75,
+                message: "I want to travel here!",
+                focus: true,
+                draggable: false
+            }
+          };
+
+      angular.extend($scope, {
+        center: {
+            lat: -10.036,
+            lng: -70,
+            zoom: 2
+        },
+
+        defaults: {
+            tileLayer: "tiles2/{z}/{x}/{y}.png",
+            tileLayerOptions: {
+                opacity: 0.9,
+                detectRetina: true,
+                reuseTiles: true,
+            },
+            scrollWheelZoom: false,
+            maxZoom: 3,
+        }
+    });
+
+  $scope.crear_marcador = function() {
+    $scope.data.markers['asdasd'] = {
+      lat: 29.91,
+      lng: 0.75,
+      message: "I want to travel here!",
+      focus: true,
+      draggable: true
+    }
+
+    console.log($scope.data);
+  }
 });
